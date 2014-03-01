7884929433c3dfc940d943af778e1e15c3f2aac6
Nathanaël Bertrand <cmoinath@gmail.com>
2014-03-01 11:31:31 +0100

    Creating of userController

---
+++ grails-app/controllers/stagetime/UserController.groovy
@@ -0,0 +1,48 @@
+package stagetime
+
+class UserController {
+
+    /**
+     * List entities
+     */
+    def index() { }
+
+    /**
+     * Get given entity
+     * @param id
+     */
+    def get(Long id){
+
+    }
+
+    /**
+     * Generate view for creating new entity
+     */
+    def create() {
+
+    }
+
+    /**
+     * generate view for editing given entity
+     * @param id
+     */
+    def edit(Long id) {
+
+    }
+
+    /**
+     * Create or update entity
+     */
+    def save() {
+
+    }
+
+    /**
+     * Delete entity defined by the given id
+     * @param id
+     */
+    def delete(Long id){
+
+    }
+
+}
